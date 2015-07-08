22ef64a4ddb32f2bf6fbd3a8f0733955613dda4b
M Sinclair Stevens <msinclairstevens@gmail.com>
2015-07-08 11:34:42 -0500

    Initial commit of graphic showing side-by-side plots in a 2x2 matrix.

---
+++ plot4.R
@@ -0,0 +1,83 @@
+#The data file is large, so read only a subset into R.
+data <- read.table("./data/household_power_consumption.txt",
+                   header=TRUE,
+                   sep=";",
+                   na.strings="?",
+                   nrows=10000,
+                   skip=62000,
+                   stringsAsFactors=FALSE,
+                   
+)
+
+#Clean: Add variable labels.
+names(data) <- c("date", "time", "global_active_power", "global_reactive_power", "voltage", "global_intensity", 
+                 "sub_metering_1", "sub_metering_2", "sub_metering_3"
+)
+
+#Subset for observations on dates of interest.
+library(dplyr)
+feb01 <-filter(data, date=="1/2/2007")
+feb02 <-filter(data, date=="2/2/2007")
+
+#Combine dates of interest and create a data table.
+library(data.table)
+plotdata <- data.table(rbind(feb01, feb02))
+
+#Clean: Transform dates. 
+library(lubridate)
+x <- paste(plotdata$date, plotdata$time)
+plotdata <- mutate(plotdata, new_datetime=dmy_hms(x))
+numdate <- as.numeric(plotdata$new_datetime)
+
+#Initiate plot.
+library(graphics)
+par(mfcol = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0)) 
+with(plotdata, {
+  plot(numdate, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
+  with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
+  with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
+  legend("topright", box.lwd=0,lty=1, lwd=1, col = c("black", "blue","red"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+})
+
+#Output plot to graphics device
+library(grDevices)
+png(filename = "plot4.png", width=480, height=480, units="px") 
+
+par(mfcol = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0)) 
+with(plotdata, {
+  plot(numdate, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
+  with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
+  with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
+  legend("topright", box.lwd=0,lty=1, lwd=1, col = c("black", "blue","red"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+  
+  plot(numdate, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
+  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+})
+dev.off() 
