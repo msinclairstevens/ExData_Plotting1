93c6863d1fd701057d704366d0ecad66f1b1a001
M Sinclair Stevens <msinclairstevens@gmail.com>
2015-07-08 16:28:48 -0500

    Correct colors in the legend.

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 #The data file is large, so read only a subset into R.
 data <- read.table("./data/household_power_consumption.txt",
-                   header=TRUE,
+                   header=FALSE,
                    sep=";",
                    na.strings="?",
                    nrows=10000,
@@ -27,31 +27,26 @@ plotdata <- data.table(rbind(feb01, feb02))
 library(lubridate)
 x <- paste(plotdata$date, plotdata$time)
 plotdata <- mutate(plotdata, new_datetime=dmy_hms(x))
-numdate <- as.numeric(plotdata$new_datetime)
 
-#Initiate plot.
+# #Initiate plot. After testing. Copy and comment out.
 library(graphics)
-with(plotdata, plot(numdate, sub_metering_1, type="l", col="black", 
-                    xaxt="n" , xlab="", ylab="Energy sub metering"))
+with(plotdata, plot(new_datetime, sub_metering_1, type="l", col="black", 
+                    xlab="", ylab="Energy sub metering"))
 
 #Annotate plot.
 ##Add more points.
-with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
-with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
-legend("topright", lty=1, lwd=1, col = c("black", "blue","red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(plotdata, points(new_datetime, sub_metering_2, type="l", col="red"))
+with(plotdata, points(new_datetime, sub_metering_3, type="l", col="blue"))
+legend("topright", lty=1, lwd=1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-##Label x axis.
-xpoints <- c(min(numdate),median(numdate),max(numdate))
-axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
 
 #Output plot to graphics device
 library(grDevices)
 png(filename = "plot3.png", width=480, height=480, units="px") 
-with(plotdata, plot(numdate, sub_metering_1, type="l", col="black", 
-                    xaxt="n" , xlab="", ylab="Energy sub metering"))
-with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
-with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
-legend("topright", lty=1, lwd=1, col = c("black", "blue","red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-xpoints <- c(min(numdate),median(numdate),max(numdate))
-axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
+with(plotdata, plot(new_datetime, sub_metering_1, type="l", col="black", 
+                    xlab="", ylab="Energy sub metering"))
+with(plotdata, points(new_datetime, sub_metering_2, type="l", col="red"))
+with(plotdata, points(new_datetime, sub_metering_3, type="l", col="blue"))
+legend("topright", lty=1, lwd=1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.off() 
