ed425dc767e2d626cfdb4bef418e7bf5bd911dab
M Sinclair Stevens <msinclairstevens@gmail.com>
2015-07-08 16:33:24 -0500

    Delete the numeric vector of new_datetime and replace all calls of it with calls to new_datetime.

--- plot4.R
+++ plot4.R
@@ -1,12 +1,11 @@
 #The data file is large, so read only a subset into R.
 data <- read.table("./data/household_power_consumption.txt",
-                   header=TRUE,
+                   header=FALSE,
                    sep=";",
                    na.strings="?",
                    nrows=10000,
                    skip=62000,
-                   stringsAsFactors=FALSE,
-                   
+                   stringsAsFactors=FALSE,                   
 )
 
 #Clean: Add variable labels.
@@ -27,30 +26,30 @@ plotdata <- data.table(rbind(feb01, feb02))
 library(lubridate)
 x <- paste(plotdata$date, plotdata$time)
 plotdata <- mutate(plotdata, new_datetime=dmy_hms(x))
-numdate <- as.numeric(plotdata$new_datetime)
+
 
 #Initiate plot.
 library(graphics)
 par(mfcol = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0)) 
 with(plotdata, {
-  plot(numdate, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
-  with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
-  with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
-  legend("topright", box.lwd=0,lty=1, lwd=1, col = c("black", "blue","red"), 
+  plot(new_datetime, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
+  with(plotdata, points(new_datetime, sub_metering_2, type="l", col="red"))
+  with(plotdata, points(new_datetime, sub_metering_3, type="l", col="blue"))
+  legend("topright", bty="n",lty=1, lwd=1, col = c("black", "red","blue"), 
          legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
 })
 
@@ -60,24 +59,24 @@ png(filename = "plot4.png", width=480, height=480, units="px")
 
 par(mfcol = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0)) 
 with(plotdata, {
-  plot(numdate, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, global_active_power, ylab="Global Active Power", type="l", xaxt="n", xlab="")
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
-  with(plotdata, points(numdate, sub_metering_2, type="l", col="red"))
-  with(plotdata, points(numdate, sub_metering_3, type="l", col="blue"))
+  plot(new_datetime, sub_metering_1, ylab="Energy sub metering", type="l", xaxt="n", xlab="")
+  with(plotdata, points(new_datetime, sub_metering_2, type="l", col="red"))
+  with(plotdata, points(new_datetime, sub_metering_3, type="l", col="blue"))
   legend("topright", box.lwd=0,lty=1, lwd=1, col = c("black", "blue","red"), 
          legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, voltage, ylab="Voltage", type="l", xaxt="n", xlab="datetime")
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
   
-  plot(numdate, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
-  xpoints <- c(min(numdate),median(numdate),max(numdate))
+  plot(new_datetime, global_reactive_power, ylab="Global_reactive_power", type="l", xaxt="n", xlab="datetime") 
+  xpoints <- c(min(new_datetime),median(new_datetime),max(new_datetime))
   axis(1, at=c(xpoints),labels=c('Thu','Fri','Sat'))
 })
 dev.off() 
